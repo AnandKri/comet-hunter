00f6e2891ada4a185cf3eebab6cebe206ac7fdfd
Anand Krishna <anand.krishna0802@gmail.com>
2026-05-02 23:54:37 +0530

    feat(scheduler): add get_status method, update methods to capture scheduler state; refactor instruments from initializing as an attribute to being passed to method directly, implementing enough decoupling

--- backend/pipeline/scheduler.py
+++ backend/pipeline/scheduler.py
@@ -2,10 +2,10 @@ from datetime import datetime, UTC, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.date import DateTrigger
 import logging
-
+from typing import Optional
 from backend.util.enums import Instrument
 from backend.pipeline.pipeline import Pipeline
-
+from backend.pipeline.models import SchedulerStatusResult, SchedulerStartResult, SchedulerShutdownResult
 
 logger = logging.getLogger(__name__)
 
@@ -15,12 +15,15 @@ class Scheduler:
     Runs only the live ingestion pipeline on a fixed interval.
     """
 
-    def __init__(self, pipeline: Pipeline, instruments: list[Instrument]):
+    def __init__(self, pipeline: Pipeline):
         self.pipeline = pipeline
-        self.instruments = instruments
         self.scheduler = BackgroundScheduler(timezone=UTC)
+        self.job_id = "run_live_pipeline_job"
+        self.next_run_at: Optional[datetime] = None
+        self.next_run_in: Optional[timedelta] = None
+        self.running = False
 
-    def _job(self):
+    def _job(self, instruments: list[Instrument]):
         """
         Executes one cycle of the live ingestion pipeline and schedules the next run.
 
@@ -37,7 +40,7 @@ class Scheduler:
         
         """
         try:
-            for instrument in self.instruments:
+            for instrument in instruments:
                 logger.info(f"instrument : {Instrument.value}")
                 
                 result = self.pipeline.run_live_pipeline(instrument)
@@ -52,18 +55,22 @@ class Scheduler:
             logger.exception(f"Scheduler job failed: {e}")
             next_run = timedelta(minutes=5)
 
-        next_time = datetime.now(UTC) + next_run
+        self.next_run_in = next_run
+        self.next_run_at = datetime.now(UTC) + next_run
 
         self.scheduler.add_job(
             self._job,
-            trigger=DateTrigger(run_date=next_time),
-            id="run_live_pipeline_job",
+            trigger=DateTrigger(run_date=self.next_run_at),
+            id=self.job_id,
             replace_existing=True,
         )
 
-    def start(self):
+    def start(self, instruments: list[Instrument]) -> SchedulerStartResult:
         """
         Starts the scheduler and triggers the first pipeline execution.
+
+        :param instruments: list of instruments for which job is to be ran
+        :return: 0 on successful execution
 
         Behavior:
         - Initializes and starts the APScheduler background scheduler
@@ -73,10 +80,29 @@ class Scheduler:
         - Subsequent executions are handled by `_job()` via self-rescheduling
         - Should be called once during application startup
         """
-        self.scheduler.start()
-        self._job()
+        if not self.running:
+            self.scheduler.start()
+            self.running = True
+            self._job(instruments)
+        
+        return SchedulerStartResult(
+            response=0
+        )
+    
+    def get_status(self) -> SchedulerStatusResult:
+        """
+        Returns scheduler state.
 
-    def shutdown(self):
+        :return: dataclass entity with running state
+        next run time and time remaining in next run
+        """
+        return SchedulerStatusResult(
+            self.running,
+            self.next_run_at,
+            self.next_run_in
+        )
+
+    def shutdown(self) -> SchedulerShutdownResult:
         """
         Stops the scheduler gracefully.
 
@@ -88,5 +114,12 @@ class Scheduler:
         - Should be called during application shutdown/cleanup
         - Ensures no background threads remain active
         """
-        self.scheduler.shutdown()
-        logger.info("Scheduler stopped")
+        if self.running:
+            self.scheduler.shutdown()
+            self.running = False
+            self.next_run_at = None
+            self.next_run_in = None
+            logger.info("Scheduler stopped")
+        return SchedulerShutdownResult(
+            response=0
+        )
