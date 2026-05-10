f3b439fcb72871db6ebb79504e3e0b290acf7504
Anand Krishna <anand.krishna0802@gmail.com>
2026-05-10 15:06:18 +0530

    normalize UTC datetime handling using parse_utc_datetime

--- backend/util/funcs.py
+++ backend/util/funcs.py
@@ -1,9 +1,27 @@
-from datetime import datetime, timezone
+from datetime import datetime, UTC
 
 def validate_time_window(start: datetime, end: datetime):
     if start >= end:
         raise ValueError("start must be earlier than end")
 
-def _to_utc(dt_str: str):
+def to_utc(dt_str: str) -> datetime:
     dt = datetime.fromisoformat(dt_str)
-    return dt if dt.tzinfo else dt.replace(tzinfo=timezone.utc)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=UTC)
+    return dt.astimezone(UTC)
+
+def parse_utc_datetime(dt_str: str) -> datetime:
+    """
+    Parse ISO datetime string into timezone-aware UTC datetime.
+
+    Rules:
+    - naive datetimes are assumed to be UTC
+    - timezone-aware datetimes are normalized to UTC
+    """
+
+    dt = datetime.fromisoformat(dt_str)
+
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=UTC)
+
+    return dt.astimezone(UTC)
