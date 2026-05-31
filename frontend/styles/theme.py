cb3c4fd55300a5cea55e78c4afba481ca8155c9d
Anand Krishna <anand.krishna0802@gmail.com>
2026-05-31 21:14:58 +0530

    refactor: centralize repititive UI designs

---
+++ frontend/styles/theme.py
@@ -0,0 +1,69 @@
+PANEL_CARD = """
+w-full
+rounded-xl
+bg-white
+border
+border-gray-200
+p-2
+shadow-sm
+gap-1
+"""
+
+PANEL_TITLE = """
+font-semibold
+text-gray-800
+test-base
+"""
+
+PRIMARY_BUTTON = """
+h-1
+px-2
+rounded-lg
+bg-blue-600
+text-white
+text-xs
+"""
+
+STATUS_BADGE = """
+text-xs
+font-semibold
+px-1
+py-1
+rounded-full
+"""
+
+ROW_BETWEEN = """
+w-full
+items-center
+justify-between
+"""
+
+NAVBAR = """
+w-full
+items-center
+justify-between
+px-6
+h-[44px]
+bg-black
+text-white
+shadow-sm
+"""
+
+NAVBAR_LINK = """
+text-base 
+text-white 
+no-underline 
+hover:text-gray-300
+"""
+
+FOOTER = """
+w-full
+items-center
+justify-center
+gap-2
+bg-black
+text-white
+h-[44px]
+text-base
+shadow-sm
+"""
